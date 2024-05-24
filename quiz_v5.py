f9b5288c149dd983652a53480fc3a8f158585cf1
dychoe2008 <dychoe2008@gmail.com>
2024-05-24 12:40:32 +1200

    Update quiz_v5.py

--- quiz_v5.py
+++ quiz_v5.py
@@ -1,17 +1,21 @@
-"""Program for a general knowledge quiz about NZ V5
+"""Program for a general knowledge quiz about NZ V5.
 This version will include component 1, 2, 3, 4 part 1 and part 2, 5
 By Daniel C
 """
 # pylint: disable = c0103
+
+
 def str_checker(string, question_01):
-    """Checks if the everything in the input is in the alpahbet"""
+    """Check if the everything in the input is in the alpahbet."""
     invalid_str = "\n Sorry, you must enter a vaild input\n"
     while string.isalpha() is False:
         print(invalid_str)
         string = str(input(question_01))
     return string
+
+
 def int_checker(question_02):
-    """Checks the input was a null or string repeats until receives a int."""
+    """Check the input was a null or string repeats until receives a int."""
     invalid = "\n You must enter an integer that's from 5 to 11\n"
     num = ""
     while not num:
@@ -20,15 +24,19 @@ def int_checker(question_02):
             return num
         except ValueError:
             print(invalid)
+
+
 def welcome_screen():
-    """Prints a welcome statement"""
+    """Print a welcome statement."""
     print()
     print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     print("Welcome to the 'General Knowledge Quiz about NZ!'")
     print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     print()
+
+
 def ask_name():
-    """Asks what their name is"""
+    """Asks what their name is."""
     invalid_length = "\nSorry please enter a valid name length\n"
     name = input("What is your name?: ").title()
     set_name = str_checker(name, "What is your name?: ").title()
@@ -37,8 +45,10 @@ def ask_name():
         name = input("What is your name?: ").title()
         set_name = str_checker(name, "What is your name?: ").title()
     return set_name
+
+
 def ask_age():
-    """Asks for their age"""
+    """Asks for their age."""
     invalid_age = "\n You must enter an whole number that's from 5 to 11\n"
     age = int_checker("What is your age?\n"
                       "(This will pick which Quiz you will be trying): ")
